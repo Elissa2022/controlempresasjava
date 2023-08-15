35d02713f3865210e491d3ed84c8871c81890957
Yanira <oc22002@esfe.agape.edu.sv>
2023-08-14 14:17:35 -1200

    Codificación de las clases DAL

--- ControlEmpresas.EntidadesDeNegocio/src/controlempresas/entidaesdenegocio/Rol.java
+++ ControlEmpresas.EntidadesDeNegocio/src/controlempresas/entidaesdenegocio/Rol.java
@@ -18,12 +18,38 @@ public class Rol {
         this.top_aux = top_aux;
         this.usuarios = usuarios;
     }
-  
 
     public int getId() {
         return id;
     }
+
     public void setId(int id) {
         this.id = id;
     }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getTop_aux() {
+        return top_aux;
+    }
+
+    public void setTop_aux(int top_aux) {
+        this.top_aux = top_aux;
+    }
+
+    public ArrayList<Usuario> getUsuarios() {
+        return usuarios;
+    }
+
+    public void setUsuarios(ArrayList<Usuario> usuarios) {
+        this.usuarios = usuarios;
+    }
+  
+
 }
