7a7b3bae16310542660b7d050c7138293654e923
Yanira <oc22002@esfe.agape.edu.sv>
2023-08-21 13:53:58 -1200

    Codificación  de las clases del paquete utils de AppWeb

--- ControlEmpresas.AppWeb/src/java/controlempresas/appweb/utils/IAuthorize.java
+++ ControlEmpresas.AppWeb/src/java/controlempresas/appweb/utils/IAuthorize.java
@@ -1,13 +1,8 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
- */
 package controlempresas.appweb.utils;
 
-/**
- *
- * @author machi
- */
+import java.io.IOException;
+import jakarta.servlet.ServletException;
+
 public interface IAuthorize {
-    
+    void authorize() throws ServletException, IOException;
 }
